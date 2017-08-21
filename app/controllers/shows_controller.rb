30cd7dd641bd44eee5ac75c0652f1f720800e269
Nathan Campbell <nathancampbell@outlook.com>
2017-08-21 11:28:24 -0500

    first shows controller completed

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,15 +7,38 @@ class ShowsController < ApplicationController
   end
 
   def new
-    @show = Show.new
+    @show = current_user.shows.build
+    @show.reviews.build
   end
 
   def create
-    @show = Show.find_with_tmdb(params[:search])
+    search = Show.find_with_tmdb(params[:search])
+    @show = current_user.show.build(search)
+    if @show.save
+      redirect_to @show, notice: 'Show successfully added'
+    else
+      render :new, notice: @show.errors.full_messages
+    end
   end
 
   def show
     @reviews = Review.where(show_id: @show.id)
+  end
+
+  def edit
+  end
+
+  def update
+    if @show.update(show_params)
+      render :show, notice: 'Show successfully updated'
+    else
+      render :edit, notice: @show.errors.full_messages
+    end
+  end
+
+  def destroy
+    @show.destroy
+    redirect_to shows_url, notice: 'Show successfully deleted'
   end
 
   private
@@ -26,18 +49,18 @@ class ShowsController < ApplicationController
 
   def show_params
     params.require(:show).permit(
-    	:name,
-    	:overview,
-    	:poster,
-    	:rating,
-    	:num_episodes,
-    	:num_seasons,
-    	:cast,
-    	:created,
-    	:network,
-    	:genres,
-    	:backdrop,
-    	reviews_attributes: [:comment]
-    	)
+      :name,
+      :overview,
+      :poster,
+      :rating,
+      :num_episodes,
+      :num_seasons,
+      :cast,
+      :created,
+      :network,
+      :genres,
+      :backdrop,
+      reviews_attributes: [:comment]
+    )
   end
 end
