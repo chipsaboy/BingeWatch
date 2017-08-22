2c03060dfc6d6d562cdaa724c5152da8dd3acc19
Nathan Campbell <nathancampbell@outlook.com>
2017-08-22 12:37:31 -0500

    starting reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+	before_action :find_review, only: [:show, :edit, :update, :destroy]
+	before_action :find_show, except: [:index, :show]
+	before_action :authenticate_user!
+
+	private
+
+	def find_review
+		@review = Review.find(params[:id])
+	end
+
+	def find_show
+		@show = Show.find(params[:show_id])
+	end
+
+	def review_params
+		params.require(:review).permit(:comment)
+	end
 end
