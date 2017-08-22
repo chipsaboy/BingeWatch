3086e7600e454585ee04856a54ae69c71b792143
Nathan Campbell <nathancampbell@outlook.com>
2017-08-22 12:46:50 -0500

    built index, new, and create for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,28 @@ class ReviewsController < ApplicationController
 	before_action :find_show, except: [:index, :show]
 	before_action :authenticate_user!
 
+	def index
+		if !params[:user_id]
+			@reviews = Review.all.order("created_at DESC")
+		else
+			@reviews = Review.all.where("user_id = ?", params[:user_id])
+		end
+	end
+
+	def new
+		@review = Review.new
+	end
+
+	def create
+		@review = @show.reviews.build(review_params)
+		@review.user = current_user
+		if @review.save
+			render @review, notice: "Review published"
+		else
+			render :new, notice: @review.errors.full_messages
+		end
+	end
+
 	private
 
 	def find_review
