54c6631e56303ba44fc68e5d6d1460fd1948afb1
Nathan Campbell <nathancampbell@outlook.com>
2017-08-24 14:57:45 -0500

    simplifying shows controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,5 @@
 class ShowsController < ApplicationController
-  before_action :find_show, only: [:edit, :update, :destroy]
+  before_action :find_show, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -12,22 +12,24 @@ class ShowsController < ApplicationController
   end
 
   def create
-    search = Show.find_info_with_tmdb(params[:show][:name])
-    @show = current_user.shows.build(show_params.merge(search))
+    @show = current_user.shows.build(show_params)
     if @show.save
-      redirect_to @show
+      redirect_to @show, notice: 'Show successfully created'
     else
       render :new, alert: @show.errors.full_messages
     end
   end
 
   def show
-    @show = Show.find_by(id: params[:id])
   	@reviews = Review.where(show_id: @show.id)
+    if @reviews.nil?
+      "No Reviews"
+    else
+      @reviews
+    end
   end
 
   def edit
-    @show = Show.find_by(id: params[:id])
   end
 
   def update
