da208dc52ff2c2c47cc741e829fc72d9e21f2cca
Nathan Campbell <nathancampbell@outlook.com>
2017-08-24 15:05:07 -0500

    working on views and deleted tmdb method from show model

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,23 +9,4 @@ class Show < ApplicationRecord
       self.reviews.build(reviews_attributes)
     end
   end
-
-  def self.find_info_with_tmdb(name)
-    q = Tmdb::TV.find(name)
-    if !q.empty?
-      id = q[0].id
-      detail = Tmdb::TV.detail(id)
-      show = {
-        name: detail["name"],
-        overview: detail["overview"],
-        poster: detail["poster_path"],
-        rating: detail["vote_average"],
-        backdrop: detail["backdrop_path"],
-        }
-      return show
-    else
-      return nil
-    end
-  end
-
 end
